ed1ccbda7ac3c5eb0edbef86ce6a2a148416fad8
snezer <fofinmax1@mail.ru>
2023-10-28 19:43:49 +0700

    Урок 2 Условия

--- Zevs1/Zevs1/Program.cs
+++ Zevs1/Zevs1/Program.cs
@@ -6,23 +6,91 @@ namespace Zevs1
     {
         static void Main(string[] args)
         {
-            bool zevs = true;
-            int a = 0;
-            long b = 1;
-            uint c = 2;
 
-            float s = 1.9F;
-            Console.Write("Введите b: > ");
-            b = long.Parse(Console.ReadLine());
-            Console.Write("Введите a: > ");
-            a = int.Parse(Console.ReadLine());
-            Console.WriteLine($"b: {b}");
-            Console.WriteLine($"c: {c}");
-            Console.WriteLine($"a: {a}");
-            Console.WriteLine($"Сумма b, c и a: {b + c + a}");
-            var ck = Console.ReadKey();
-            Console.WriteLine($"{ck.KeyChar}");
-            Console.ReadKey();
+            static void Main(string[] args)
+            {
+                var d = MyRound2();
+                var r = MyRound(112.849, 1);
+                Console.WriteLine($"Значение d: {d}");
+                Console.WriteLine($"Значение r: {r}");
+                double y = 12f / 221f;
+                long x = 12 % 221;
+                Console.WriteLine($"Значение y: {y}");
+                Console.WriteLine($"Значение x: {x}");
+
+                Console.WriteLine("----------------------------------------------");
+                Condition("");
+                Condition("Зевс");
+                Condition("25.4");
+                Condition("25");
+                ConditionErr("Зевс");
+                ConditionErr("28");
+                ConditionSwitch(0);
+                ConditionSwitch(1);
+                ConditionSwitch(2);
+            }
+
+            static double MyRound(double valyok, int dec)
+            {
+                double result = Math.Round(valyok, dec);
+                return result;
+            }
+
+            static double MyRound2()
+            {
+                return Math.Round(112.849, 1);
+            }
+
+            static void ConditionErr(string s)
+            {
+                try
+                {
+                    var i = int.Parse(s);
+                    Console.WriteLine($"Значение i: {i}");
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine($"Ошибка преобразования: {err}");
+                    //throw;
+                }
+                finally
+                {
+                    Console.WriteLine("Завершение выполнения метода ConditionErr");
+                }
+            }
+
+
+            static void Condition(string s)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    Console.WriteLine("Строка пустая!");
+                }
+                else if (int.TryParse(s, out int result))
+                {
+                    Console.WriteLine($"Вы ввели число: {result}");
+                }
+                else
+                {
+                    Console.WriteLine($"Строка: {s}");
+                }
+            }
+
+            static void ConditionSwitch(int u)
+            {
+                switch (u)
+                {
+                    case 1:
+                        Console.WriteLine("Вы ввели 1.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Вы ввели 2.");
+                        break;
+                    default:
+                        Console.WriteLine("вы ввели какую-то хуйню!");
+                        break;
+                }
+            }
         }
     }
 }
